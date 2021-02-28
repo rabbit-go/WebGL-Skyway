b30256c5c972e67d4fdde209898c4e4b15e24ff8
kikuchi yusuke <yusuke.kikuchi@outlook.com>
2021-02-28 13:11:28 +0900

    Mod SkyWay

--- Build/Scripts/SkyWay.js
+++ Build/Scripts/SkyWay.js
@@ -83,3 +83,25 @@ function addVideo(call, stream) {
 function removeVideo(peerId) {
     $('#their-video').get(0).srcObject = undefined;
 }
+
+// Unityと連携するための関数群
+hoge = function() {
+    return {
+        // Unityからのメッセージを受け取るハンドラ登録
+        InitializationEventListener: function() {
+            window.addEventListener('message', function(event) {
+                hoge.ExecuteJs(event.data);
+              }, false);
+          },
+        // 受け取ったメッセージから、evalを使って関数を呼び出す
+        ExecuteJs: function(message) {
+            if (typeof (message) !== "string" && !(message instanceof String) || message == "null") {
+                return;
+            }
+            var parameterObject = JSON.parse(message);
+            var methodName = parameterObject.MethodName;
+            var evalString = methodName + '(parameterObject)';
+            eval(evalString);
+          }
+      };
+  }();
