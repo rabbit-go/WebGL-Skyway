0616b253dc2df6079663758c4bf7c77156cb0f68
kikuchi yusuke <yusuke.kikuchi@outlook.com>
2021-02-28 16:14:48 +0900

    Change Arg System

--- Build/Web.framework.js
+++ Build/Web.framework.js
@@ -1297,7 +1297,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2232448;
+STATICTOP = STATIC_BASE + 2231792;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3275,7 +3275,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2232448;
+var STATIC_BUMP = 2231792;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
