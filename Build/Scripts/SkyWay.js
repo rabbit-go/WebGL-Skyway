649db45b3a44cecb898ee1ebc6416f2c7ce20920
kikuchi yusuke <yusuke.kikuchi@outlook.com>
2021-03-02 16:14:58 +0900

    mod

--- Build/Scripts/SkyWay.js
+++ Build/Scripts/SkyWay.js
@@ -8,7 +8,10 @@ const VIDEO_CODEC = 'VP9';
 function CreateVideoElement(id) {
     let s = document.createElement("video");
     s.setAttribute('id', id);
+    s.setAttribute('width', '400px');
+    s.setAttribute('autoplay');
     document.body.appendChild(s);
+    s.style.display='none';
 }
 //peeridを取得 
 function GetPeerId(yourid) {
