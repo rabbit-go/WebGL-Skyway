a9164f88a30e84994b3609a5d0516ab36c619dd4
kikuchi yusuke <yusuke.kikuchi@outlook.com>
2021-03-07 21:29:30 +0900

    FixAyame

--- Scripts/Ayame.js
+++ Scripts/Ayame.js
@@ -43,7 +43,7 @@ function MakeCallRight() {
     options.audio.direction = 'recvonly';
     options.signalingKey = 'YxBUizkGKEg-ydXX_M4C1ILrP606cTJKBfN-0DHdaUCdrILQ';
     const startConn = async () => {
-        const conn = Ayame.connection(signalingurl, 'rabbit-go@right, options, true);
+        const conn = Ayame.connection(signalingurl, 'rabbit-go@right', options, true);
         existingRightCall = conn;
         await conn.connect(null);
         conn.on('disconnect', (e) => {
