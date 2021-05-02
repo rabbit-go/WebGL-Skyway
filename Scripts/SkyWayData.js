b67ed7afe101771a7bea4ba7fbfee76574940877
kikuchi yusuke <yusuke.kikuchi@outlook.com>
2021-05-02 15:26:44 +0900

    asd

--- Scripts/SkyWayData.js
+++ Scripts/SkyWayData.js
@@ -1,14 +1,11 @@
 //初期化(cssでもできるはず)
 var existingDataConn = null;
+let g_theirId =""  ;
 function DataConnect(theirId) {
+    g_theirId = theirId;
     if(peer == null){
-         peer = new Peer(theirId, {
-            key: '829682c4-f853-4d97-8691-aa0c10064efd',    //APIkey
-            debug: 3
-        });
+         return;
     }
-
-
     //接続
     const conn = peer.connect(theirId);
     existingDataConn = conn;
@@ -18,6 +15,10 @@ function DataClose(theirId) {
 }
 //送信処理
 function DataSend(msg) {
+    if(existingDataConn==null){
+        DataConnect(g_theirId);
+        if(existingDataConn==null)return;
+    }
     existingDataConn.send(msg);
 }
 function DataReceived(){
