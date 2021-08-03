8761325e5cdbe92e507214e386469bec32b07925
kikuchi yusuke <yusuke.kikuchi@outlook.com>
2021-08-03 16:38:43 +0900

    asd

--- Scripts/Audio/3Daudio.js
+++ Scripts/Audio/3Daudio.js
@@ -2,145 +2,94 @@
 // Super version: http://chromium.googlecode.com/svn/trunk/samples/audio/simple.html
 
 class PositionSample {
+    audioCtx = new (window.AudioContext)();
+    isPlaying = false;
     createPanner(stream) {
-        // Hook up the audio graph for this sample.
-        var source = document.audioContext.createMediaStreamSource(stream);
-        var panner = document.audioContext.createPanner();
-        panner.coneOuterGain = 0.1;
-        panner.coneOuterAngle = 180;
-        panner.coneInnerAngle = 0;
-        // Set the panner node to be at the origin looking in the +x
-        // direction.
-        panner.connect(document.audioContext.destination);
-        source.connect(panner);
-        source.start(0);
-        // Position the listener at the origin.
-        document.audioContext.listener.setPosition(0, 0, 0);
-        return panner;
+      // Hook up the audio graph for this sample.
+      var source = this.audioCtx.createMediaStreamSource(stream);
+      var panner = this.audioCtx.createPanner();
+      panner.coneOuterGain = 0.1;
+      panner.coneOuterAngle = 180;
+      panner.coneInnerAngle = 0;
+      // Set the panner node to be at the origin looking in the +x
+      // direction.
+      panner.connect(this.audioCtx.destination);
+      source.connect(panner);
+      // Position the listener at the origin.
+      this.audioCtx.listener.setPosition(0, 0, 0);
+      return panner;
     }
-
-    async createStereoPanner(stream) {
-        // Hook up the audio graph for this sample.
-        var source = document.audioContext.createMediaStreamSource(stream);
-        var data = await document.audioContext.decodeAudioData(someStereoBuffer);
-        var sourceL = ac.createBufferSource();
-        var sourceR = ac.createBufferSource();
-        sourceL.buffer = data;
-        sourceR.buffer = data;
-        var splitter = ac.createChannelSplitter(2);
-        sourceL.connect(splitter);
-        var merger = ac.createChannelMerger(2);
-
-        // 左チャンネルのボリュームのみ小さくする
-        var gain = ac.createGain();
-        gain.value = 0.5;
-        splitter.connect(gain, 0);
-
-        // splitterをmergerの2番目の入力にして戻す
-        // ここではチャンネルを入れ替えることで、ステレオ音声の左右を逆にしている
-        gain.connect(merger, 0, 1);
-        splitter.connect(merger, 1, 0);
-
-        var dest = ac.createMediaStreamDestination();
-
-        // ChannelMergerNodeを使ったのでステレオのMediaStreamとなった
-        // webオーディオグラフのWebRTCやMediaRecorderなどに渡す
-        merger.connect(dest);
-
-
-
-        var panner = document.audioContext.createPanner();
-        panner.coneOuterGain = 0.1;
-        panner.coneOuterAngle = 180;
-        panner.coneInnerAngle = 0;
-        // Set the panner node to be at the origin looking in the +x
-        // direction.
-        panner.connect(document.audioContext.destination);
-        source.connect(panner);
-        source.start(0);
-        // Position the listener at the origin.
-        document.audioContext.listener.setPosition(0, 0, 0);
-        return panner;
-    }
-
+  
     changePosition(panner, position) {
-        // Position coordinates are in normalized canvas coordinates
-        // with -0.5 < x, y < 0.5
-        if (position) {
-            if (!this.isPlaying) {
-                this.play();
-            }
-            panner.setPosition(position.x, position.y, -0.5);
-        } else {
-            this.stop();
-        }
+      panner.setPosition(position.x, position.y, -0.5);
     };
     changeAngle(panner, angle) {
-        //  console.log(angle);
-        // Compute the vector for this angle.
-        panner.setOrientation(Math.cos(angle), -Math.sin(angle), 1);
+      //  console.log(angle);
+      // Compute the vector for this angle.
+      panner.setOrientation(Math.cos(angle), -Math.sin(angle), 1);
     };
-}
-
-
-function startStream() {
+  }
+  
+  
+  function startStream() {
     const peer = new Peer('client', { key: '829682c4-f853-4d97-8691-aa0c10064efd' });
     const streams = new Array(4);
     peer.on("open", x => {
-        const roomRF = peer.joinRoom("micRF", { mode: "mesh", audioReceiveEnabled: true, videoReceiveEnabled: false, stream: null });
-        const roomRB = peer.joinRoom("micRB", { mode: "mesh", audioReceiveEnabled: true, videoReceiveEnabled: false, stream: null });
-        const roomLF = peer.joinRoom("micLF", { mode: "mesh", audioReceiveEnabled: true, videoReceiveEnabled: false, stream: null });
-        const roomLB = peer.joinRoom("micLB", { mode: "mesh", audioReceiveEnabled: true, videoReceiveEnabled: false, stream: null });
-        roomRF.on("stream", stream => {
-            if (stream != null) {
-                streams[0] = stream;
-                StartDirectionAudio(streams);
-            }
-        });
-        roomRB.on("stream", stream => {
-            if (stream != null) {
-                streams[1] = stream;
-                StartDirectionAudio(streams);
-            }
-
-        });
-        roomLF.on("stream", stream => {
-            if (stream != null) {
-                streams[2] = stream;
-                StartDirectionAudio(streams);
-            }
-
-        });
-        roomLB.on("stream", stream => {
-            if (stream != null) {
-                streams[3] = stream;
-                StartDirectionAudio(streams);
-            }
-
-        });
+      const roomRF = peer.joinRoom("micRF", { mode: "mesh", audioReceiveEnabled: true, videoReceiveEnabled: false, stream: null });
+      const roomRB = peer.joinRoom("micRB", { mode: "mesh", audioReceiveEnabled: true, videoReceiveEnabled: false, stream: null });
+      const roomLF = peer.joinRoom("micLF", { mode: "mesh", audioReceiveEnabled: true, videoReceiveEnabled: false, stream: null });
+      const roomLB = peer.joinRoom("micLB", { mode: "mesh", audioReceiveEnabled: true, videoReceiveEnabled: false, stream: null });
+      roomRF.on("stream", stream => {
+        if (stream != null) {
+          streams[0] = stream;
+          StartDirectionAudio(streams);
+        }
+      });
+      roomRB.on("stream", stream => {
+        if (stream != null) {
+          streams[1] = stream;
+          StartDirectionAudio(streams);
+        }
+  
+      });
+      roomLF.on("stream", stream => {
+        if (stream != null) {
+          streams[2] = stream;
+          StartDirectionAudio(streams);
+        }
+  
+      });
+      roomLB.on("stream", stream => {
+        if (stream != null) {
+          streams[3] = stream;
+          StartDirectionAudio(streams);
+        }
+  
+      });
     });
-}
-function StartDirectionAudio(streams) {
+  }
+  function StartDirectionAudio(streams) {
     for (let i = 0; i < streams.length; i++) {
-        if (streams[i] == null) { return; }
+      if (streams[i] == null) { return; }
     }
     Create4DirectionAudio(streams);
-}
-var positionSample = new PositionSample();
-function Create4DirectionAudio(streams) {
-
+  }
+  var positionSample = new PositionSample();
+  function Create4DirectionAudio(streams) {
+  
     for (let i = 0; i < streams.length; i++) {
-        var panner = positionSample.createPanner(streams[i]);
-        let position = new Vector2(cos(90 + (90 * i)), sin(90 + (90 * i)))
-        let angle = 180 + (90 * i);
-        positionSample.changePosition(panner, position);
-        positionSample.changeAngle(panner, angle);
+      var panner = positionSample.createPanner(streams[i]);
+      let position = new Vector2(cos(90 + (90 * i)), sin(90 + (90 * i)))
+      let angle = 180 + (90 * i);
+      positionSample.changePosition(panner, position);
+      positionSample.changeAngle(panner, angle);
     }
-}
-function setPosition(x, y) {
+  }
+  function setPosition(x, y) {
     context.listener.setPosition(x, y, 0);
-}
-function setOrientation(x, y) {
+  }
+  function setOrientation(x, y) {
     myListener.setOrientation(x, y, 0, 0, 0, 1);
-}
-
+  }
+  
+  
