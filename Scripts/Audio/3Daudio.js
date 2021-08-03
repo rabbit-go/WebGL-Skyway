977bccad97b6b6f2399079176b2fd9d841660ccc
kikuchi yusuke <yusuke.kikuchi@outlook.com>
2021-08-03 21:52:58 +0900

    asd

--- Scripts/Audio/3Daudio.js
+++ Scripts/Audio/3Daudio.js
@@ -38,7 +38,7 @@ class Vector2 {
 
       // Set the panner node to be at the origin looking in the +x
       // direction.
-      source.connect(panner).connect(document.audioContext.destination);
+      source.connect(document.audioContext.destination);
       // Position the listener at the origin.
       this.audioCtx.listener.setPosition(0, 0, 0);
       return panner;
