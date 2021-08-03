d355ca4fea1c089efc638d267bc337d158cde398
kikuchi yusuke <yusuke.kikuchi@outlook.com>
2021-08-03 16:11:41 +0900

    asd

--- Scripts/Audio/3Daudio.js
+++ Scripts/Audio/3Daudio.js
@@ -1,65 +1,6 @@
 
 // Super version: http://chromium.googlecode.com/svn/trunk/samples/audio/simple.html
-function startStream() {
-    const peer = new Peer('client', { key: '829682c4-f853-4d97-8691-aa0c10064efd' });
-    const streams = new Array(4);
-    peer.on("open", x => {
-        const roomRF = peer.joinRoom("micRF", { mode: "mesh", audioReceiveEnabled: true, videoReceiveEnabled: false, stream: null });
-        const roomRB = peer.joinRoom("micRB", { mode: "mesh", audioReceiveEnabled: true, videoReceiveEnabled: false, stream: null });
-        const roomLF = peer.joinRoom("micLF", { mode: "mesh", audioReceiveEnabled: true, videoReceiveEnabled: false, stream: null });
-        const roomLB = peer.joinRoom("micLB", { mode: "mesh", audioReceiveEnabled: true, videoReceiveEnabled: false, stream: null });
-        roomRF.on("stream", stream => {
-            if (stream != null) {
-                streams[0] = stream;
-                StartDirectionAudio();
-            }
-        });
-        roomRB.on("stream", stream => {
-            if (stream != null) {
-                streams[1] = stream;
-                StartDirectionAudio();
-            }
 
-        });
-        roomLF.on("stream", stream => {
-            if (stream != null) {
-                streams[2] = stream;
-                StartDirectionAudio();
-            }
-
-        });
-        roomLB.on("stream", stream => {
-            if (stream != null) {
-                streams[3] = stream;
-                StartDirectionAudio();
-            }
-
-        });
-    });
-}
-function StartDirectionAudio() {
-    for (let i = 0; i < streams.length; i++) {
-        if (streams[i] == null) { return; }
-    }
-    Create4DirectionAudio(streams);
-}
-var positionSample = new PositionSample();
-function Create4DirectionAudio(streams) {
-
-    for (let i = 0; i < streams.length; i++) {
-        var panner = positionSample.createPanner(streams[i]);
-        let position = new Vector2(cos(90 + (90 * i)), sin(90 + (90 * i)))
-        let angle = 180 + (90 * i);
-        positionSample.changePosition(panner, position);
-        positionSample.changeAngle(panner, angle);
-    }
-}
-function setPosition(x, y) {
-    context.listener.setPosition(x, y, 0);
-}
-function setOrientation(x, y) {
-    myListener.setOrientation(x, y, 0, 0, 0, 1);
-}
 class PositionSample {
     createPanner(stream) {
         // Hook up the audio graph for this sample.
@@ -140,3 +81,66 @@ class PositionSample {
         panner.setOrientation(Math.cos(angle), -Math.sin(angle), 1);
     };
 }
+
+
+function startStream() {
+    const peer = new Peer('client', { key: '829682c4-f853-4d97-8691-aa0c10064efd' });
+    const streams = new Array(4);
+    peer.on("open", x => {
+        const roomRF = peer.joinRoom("micRF", { mode: "mesh", audioReceiveEnabled: true, videoReceiveEnabled: false, stream: null });
+        const roomRB = peer.joinRoom("micRB", { mode: "mesh", audioReceiveEnabled: true, videoReceiveEnabled: false, stream: null });
+        const roomLF = peer.joinRoom("micLF", { mode: "mesh", audioReceiveEnabled: true, videoReceiveEnabled: false, stream: null });
+        const roomLB = peer.joinRoom("micLB", { mode: "mesh", audioReceiveEnabled: true, videoReceiveEnabled: false, stream: null });
+        roomRF.on("stream", stream => {
+            if (stream != null) {
+                streams[0] = stream;
+                StartDirectionAudio();
+            }
+        });
+        roomRB.on("stream", stream => {
+            if (stream != null) {
+                streams[1] = stream;
+                StartDirectionAudio();
+            }
+
+        });
+        roomLF.on("stream", stream => {
+            if (stream != null) {
+                streams[2] = stream;
+                StartDirectionAudio();
+            }
+
+        });
+        roomLB.on("stream", stream => {
+            if (stream != null) {
+                streams[3] = stream;
+                StartDirectionAudio();
+            }
+
+        });
+    });
+}
+function StartDirectionAudio() {
+    for (let i = 0; i < streams.length; i++) {
+        if (streams[i] == null) { return; }
+    }
+    Create4DirectionAudio(streams);
+}
+var positionSample = new PositionSample();
+function Create4DirectionAudio(streams) {
+
+    for (let i = 0; i < streams.length; i++) {
+        var panner = positionSample.createPanner(streams[i]);
+        let position = new Vector2(cos(90 + (90 * i)), sin(90 + (90 * i)))
+        let angle = 180 + (90 * i);
+        positionSample.changePosition(panner, position);
+        positionSample.changeAngle(panner, angle);
+    }
+}
+function setPosition(x, y) {
+    context.listener.setPosition(x, y, 0);
+}
+function setOrientation(x, y) {
+    myListener.setOrientation(x, y, 0, 0, 0, 1);
+}
+
