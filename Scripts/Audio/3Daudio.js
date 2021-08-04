9f5fa68514a3a32cd1f7b60b727e620b2c5835b4
kikuchi yusuke <yusuke.kikuchi@outlook.com>
2021-08-04 11:34:41 +0900

    asd

--- Scripts/Audio/3Daudio.js
+++ Scripts/Audio/3Daudio.js
@@ -18,7 +18,7 @@ class PositionSample {
         var audio = document.createElement('audio');
         document.head.appendChild(audio);
         audio.srcObject = stream;
-        audio.muted = true;
+        //audio.muted = true;
         // Hook up the audio graph for this sample.
         const source = new MediaStreamAudioSourceNode(document.audioContext, { mediaStream: stream });
         const panner = new PannerNode(document.audioContext, { panningModel: "HRTF" });
@@ -28,7 +28,7 @@ class PositionSample {
 
         panner.panningModel = 'HRTF';
         panner.distanceModel = 'inverse';
-        panner.refDistance = 1;
+        panner.refDistance = 3;
         panner.maxDistance = 10000;
         panner.rolloffFactor = 1;
         panner.coneInnerAngle = 360;
