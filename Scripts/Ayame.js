2c68aeea5c488aa3c742d5dcdca5b52546814968
rabbitbooster <cyber.null.rabbit@gmail.com>
2021-08-18 14:14:21 +0900

    asd

--- Scripts/Ayame.js
+++ Scripts/Ayame.js
@@ -1,68 +1,65 @@
-'use strict';
-let peer = null;
-let existingLeftCall = null;
-let existingRightCall = null;
-const receiveOnly = true;    //受信専用かどうか
-const VIDEO_CODEC = 'VP9';
-const signalingurl = 'wss://ayame-labo.shiguredo.jp/signaling';
-function CreateVideoElement(id) {
-    let s = document.createElement("video");
-    s.setAttribute('id', id);
-    s.setAttribute('width', '1920px');
-    s.setAttribute('height', '1080px');
-    document.body.appendChild(s);
-    s.setAttribute('autoplay', '');
-    s.setAttribute('muted', '');
-   // s.style.display = 'none';
-    s.style.position = 'relative';
-    s.style.top = '2000px';
-    s.style.left = '150px';
+const channelId = 'rabbit-go@twincam';
+const debug = false;
+var sora = null;
+const options = {
+    videoBitRate: 15000
 }
-var S = "0123456789";
-var options;
-function MakeCallLeft() {
-    options = Ayame.defaultOptions;
-    options.video.codec = 'VP9';
-    options.video.direction = 'recvonly';
-    options.audio.direction = 'recvonly';
-    options.signalingKey = 'YxBUizkGKEg-ydXX_M4C1ILrP606cTJKBfN-0DHdaUCdrILQ';
-    const startConn = async () => {
-        const conn = Ayame.connection(signalingurl, 'rabbit-go@left', options, true);
-        existingLeftCall = conn;
-        await conn.connect(null);
-        conn.on('disconnect', (e) => {
-            console.log(e);
-            existingLeftCall = null;
-        });
-        conn.on('addstream', (e) => {
-            document.getElementById('LeftEye-video').srcObject = e.stream;
-        });
-    };
-    startConn();
+var recvonlyL;
+var recvonlyR;
+function MakeCall(yourid) {
+    recvonlyL = MakeCallfunc(yourid, "left");
+    recvonlyR = MakeCallfunc(yourid, "right");
+    recvonlyL.on("track", (event) => {
+        const stream = event.streams[0];
+        let video = document.getElementById('LeftEye-video');
+        video.srcObject = stream;
 
+    });
+    recvonlyL.on("removetrack", (event) => {
+        let video = document.getElementById('LeftEye-video');
+        video.srcObject = null;
+
+    });
+    recvonlyR.on("track", (event) => {
+        const stream = event.streams[0];
+        let video = document.getElementById('RightEye-video');
+        video.srcObject = stream;
+
+    });
+    recvonlyR.on("removetrack", (event) => {
+        let video = document.getElementById('RightEye-video');
+        video.srcObject = null;
+
+    });
 }
-function MakeCallRight() {
-    options = Ayame.defaultOptions;
-    options.video.direction = 'recvonly';
-    options.audio.direction = 'recvonly';
-    options.signalingKey = 'YxBUizkGKEg-ydXX_M4C1ILrP606cTJKBfN-0DHdaUCdrILQ';
-    const startConn = async () => {
-        const conn = Ayame.connection(signalingurl, 'rabbit-go@ri', options, true);
-        existingRightCall = conn;
-        await conn.connect(null);
-        conn.on('disconnect', (e) => {
-            console.log(e);
-            existingRightCall = null;
-        });
-        conn.on('addstream', (e) => {
-            document.getElementById('RightEye-video').srcObject = e.stream;
-        });
-    };
-    startConn();
+function MakeCallfunc(yourid, camerastr) {
+    let recvonly;
+    const conn = Ayame.connection('wss://ayame-labo.shiguredo.jp/signaling', channelId + camerastr);
+    conn.options.video.direction = 'recvonly';
+    conn.options.audio.direction = 'recvonly';
+    conn.options.signalingKey = 'YxBUizkGKEg-ydXX_M4C1ILrP606cTJKBfN-0DHdaUCdrILQ';
+    conn.connect(null);
+    return conn;
 }
+function GetPersonList(id) {
+    var element = document.getElementById(id);
+    element.innerText = "";
+    if (existingLeftCall == null) return;
+    if (existingLeftCall.members == null) return;
+    var i = 0;
+    existingLeftCall.members.forEach(menber => {
+        i++;
+        element.innerText = i + '人';
+    });
+}
+
 
 //切断処理
 function EndCall() {
-    if (existingLeftCall) existingLeftCall.disconnect();
-    if (existingRightCall) existingRightCall.disconnect();
+    recvonlyL.disconnect()
+        .then(function () {
+        });
+    recvonlyR.disconnect()
+        .then(function () {
+        });
 }
