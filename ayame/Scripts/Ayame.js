47075588e396d14cb2a286e7cbafed33d0c73279
kikuchi yusuke <yusuke.kikuchi@outlook.com>
2021-08-23 18:11:51 +0900

    asd

---
+++ ayame/Scripts/Ayame.js
@@ -0,0 +1,66 @@
+const channelId = 'twincam';
+const debug = false;
+var sora = null;
+const options = {
+    videoBitRate: 15000
+}
+var recvonlyL;
+var recvonlyR;
+function MakeCall(yourid) {
+    recvonlyL = MakeCallfunc(yourid, "left");
+    recvonlyR = MakeCallfunc(yourid, "right");
+    recvonlyL.on("addstream", (event) => {
+        const stream = event.stream;
+        let video = document.getElementById('LeftEye-video');
+        video.srcObject = stream;
+
+    });
+    recvonlyL.on("disconnect", (event) => {
+        let video = document.getElementById('LeftEye-video');
+        video.srcObject = null;
+
+    });
+    recvonlyR.on("addstream", (event) => {
+        const stream = event.stream;
+        let video = document.getElementById('RightEye-video');
+        video.srcObject = stream;
+
+    });
+    recvonlyR.on("disconnect", (event) => {
+        let video = document.getElementById('RightEye-video');
+        video.srcObject = null;
+        
+    });
+}
+function MakeCallfunc(yourid, camerastr) {
+    let recvonly;
+    const conn = Ayame.connection('wss://ayameserver.softether.net/signaling', channelId + camerastr);
+    conn.options.video.direction = 'recvonly';
+    conn.options.video.codec = 'VP9';
+    conn.options.audio.direction = 'recvonly';
+    conn.options.signalingKey = 'YxBUizkGKEg-ydXX_M4C1ILrP606cTJKBfN-0DHdaUCdrILQ';
+    conn.connect(null);
+    return conn;
+}
+function GetPersonList(id) {
+    var element = document.getElementById(id);
+    element.innerText = "";
+    if (existingLeftCall == null) return;
+    if (existingLeftCall.members == null) return;
+    var i = 0;
+    existingLeftCall.members.forEach(menber => {
+        i++;
+        element.innerText = i + '人';
+    });
+}
+
+
+//切断処理
+function EndCall() {
+    recvonlyL.disconnect()
+        .then(function () {
+        });
+    recvonlyR.disconnect()
+        .then(function () {
+        });
+}
---
+++ ayame/Scripts/SoraData.js
@@ -0,0 +1,18 @@
+//初期化(cssでもできるはず)
+var existingDataConn = null;
+let g_theirId =""  ;
+function DataConnect(theirId) {
+    
+} 
+function DataClose(theirId) {
+    
+}
+function DataRegistered(id){
+  
+}
+//送信処理
+function DataSend(msg) {
+}
+function DataReceived(){
+    
+}
