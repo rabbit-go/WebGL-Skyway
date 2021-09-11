c630fddf15d691b6ffa14ea31eda02cabe64f956
kikuchi yusuke <yusuke.kikuchi@outlook.com>
2021-09-11 22:57:34 +0900

    asd

--- Scripts/XRSetting.js
+++ Scripts/XRSetting.js
@@ -2,6 +2,7 @@ function EnterXRSession() {
     navigator.xr.isSessionSupported('immersive-ar')
         .then((isSupported) => {
             if (isSupported) {
+                document.body.isXR = true;
                 document.getElementById("enterar").click();
                 return;
             }
@@ -9,6 +10,7 @@ function EnterXRSession() {
                 navigator.xr.isSessionSupported('immersive-vr')
                     .then((isSupported2) => {
                         if (isSupported2) {
+                            document.body.isXR = true;
                             document.getElementById("entervr").click();
                             return;
                         }
