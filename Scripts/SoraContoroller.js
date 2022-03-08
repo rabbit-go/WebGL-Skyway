910697b6331de2f6f0e9fad1f9ca304129c2de74
kikuchi yusuke <yusuke.kikuchi@outlook.com>
2022-03-08 16:33:26 +0900

    asd

--- Scripts/SoraContoroller.js
+++ Scripts/SoraContoroller.js
@@ -1,8 +1,10 @@
 function soraSendDataChannelLoop(ang_str, lin_str) {
     let ang = parseFloat(ang_str);
+    ang = 127 * ang;
+    lin = 127 * lin;
     let lin = parseFloat(lin_str);
     let video = document.getElementById('LeftEye-video');
     if (recvonlyDataChannel != null && video.srcObject != null)
         // let send_value = new Int16Array([((127*ang) << 8) & 0xff00; | (127*lin) & 0x00ff]);
-        recvonlyDataChannel.sendMessage("#sora-devtools", new Int32Array([0x43000000 | (((127 * ang) << 8) & 0x0000ff00) | ((127 * lin) & 0x000000ff)]));
+        recvonlyDataChannel.sendMessage("#sora-devtools", new Uint8Array[0x43, 0x00, ang, lin]);
 }
