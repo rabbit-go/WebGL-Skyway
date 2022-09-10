96c6722a5400bc0008acbb778dd22eb56448e312
rabbitbooster <yusuke.kikuchi@gmail.com>
2022-09-10 16:44:09 +0900

    Update SoraContoroller.js

--- Scripts/SoraContoroller.js
+++ Scripts/SoraContoroller.js
@@ -4,9 +4,9 @@ function soraSendDataChannelLoop(ang_str, lin_str) {
     let lin = parseFloat(lin_str);
     lin = 127 * lin;
     let video = document.getElementById('LeftEye-video');
-    if (recvonlyDataChannel != null && video.srcObject != null)
+    if (recvonlyR_mobile != null && video.srcObject != null)
         // let send_value = new Int16Array([((127*ang) << 8) & 0xff00; | (127*lin) & 0x00ff]);
-        recvonlyDataChannel.sendMessage("#sora-devtools", new Uint8Array([0x43, 0x00, ang, lin]));
+        recvonlyR_mobile.sendMessage("#sora-devtools", new Uint8Array([0x43, 0x00, ang, lin]));
 }
 
 function SoraSendData(x, y, z,id) {
