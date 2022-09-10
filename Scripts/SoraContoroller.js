c47c1bbb36d30ef6091250ff0fa075734f632af8
rabbitbooster <yusuke.kikuchi@gmail.com>
2022-09-10 18:00:10 +0900

    Update SoraContoroller.js

--- Scripts/SoraContoroller.js
+++ Scripts/SoraContoroller.js
@@ -6,7 +6,7 @@ function soraSendDataChannelLoop(ang_str, lin_str) {
     let video = document.getElementById('RightEye-video-mobile');
     if (recvonlyR_mobile != null && video.srcObject != null)
         // let send_value = new Int16Array([((127*ang) << 8) & 0xff00; | (127*lin) & 0x00ff]);
-        recvonlyR_mobile.sendMessage("#sora-devtools", new Uint8Array([0x43, 0x00, ang, lin]));
+       // recvonlyR_mobile.sendMessage("#sora-devtools", new Uint8Array([0x43, 0x00, ang, lin]));
 }
 
 function SoraSendData(x, y, z,id) {
@@ -20,8 +20,36 @@ function SoraSendData(x, y, z,id) {
 }
 function SoraVRSendData(x, y, z,id) {
     let yVal = parseFloat(y);
+    twincam_setPos_count+=6;
     let yValInteger = Math.trunc((yVal/180)*127);
     let video = document.getElementById('RightEye-video-mobile');
-    if (recvonlyR_mobile  != null && video.srcObject != null)
-        recvonlyR_mobile.sendMessage("#sora-devtools", new Uint8Array([0xe0, yValInteger]));
+    if (recvonlyR_mobile  != null && video.srcObject != null){
+        HMD_ang = -yVal/Math.PI*700;
+        if (Math.abs(HMD_ang) < 350) {
+            setPosA = 0;
+        }
+        if (HMD_ang - old_HMD_ang < -700) {
+            setPosA +=  1400;
+        }
+        else if (HMD_ang - old_HMD_ang > 700) {
+            setPosA += -1400;
+        }
+        setPos = HMD_ang + setPosA;
+        if (setPos > 1050) {
+            setPos = 1050;
+        }
+        else if (setPos < -1050) {
+            setPos = -1050;
+        }
+        old_HMD_ang = HMD_ang;
+        let send_value = setPos;
+
+        if (twincam_setPos_count > 15) {
+            twincam_setPos_count = 0;
+            //recvonlyR_mobile.sendMessage("#sora-devtools", new Uint8Array([0xe0, yValInteger]));
+            recvonlyR_mobile.sendMessage("#sora-devtools", new Uint8Array([0xe0, yValInteger]));.sendMessage('#sora-devtools', new Uint8Array([0xe0, new Uint16Array([send_value & 0xff00])[0] >> 8, send_value & 0x00ff]));
+        }
+    
+    }
+        
 }
